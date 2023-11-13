928bae95d418d88ba3721d9afae3a61694e7d527
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-13 10:08:03 +0100

    userId association when creating a post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -3,8 +3,10 @@ const Post = require("../models/Post");
 const PostController = {
   async create(req, res) {
     try {
-      const post = await Post.create(req.body);
-
+      const post = await Post.create({
+        ...req.body,
+        userId: req.user._id,
+      });
       res.status(201).send(post);
     } catch (error) {
       console.error(error);
