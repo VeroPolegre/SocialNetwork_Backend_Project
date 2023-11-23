c44b4e8bc4afe2479d390973c8472858047dcdee
VeroPolegre <veronicapolegre98@gmail.com>
2023-11-23 13:37:19 +0100

    cors implementation

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
 
 const { handleTypeError } = require("./middleware/errors");
 const { dbConnection } = require("./config/config");
 
 dbConnection();
 app.use(express.json());
+app.use(cors());
 
 app.use("/posts", require("./routes/posts"));
 app.use("/users", require("./routes/users"));
