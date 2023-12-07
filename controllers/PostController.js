7cc82b947c21f3418f6adc0b534f572d5d8670af
Frances Morales Velilla <frances0688@gmail.com>
2023-12-07 12:29:45 +0100

    Update post schema and controllers.  Change multer settings to accept multiple images.

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -3,151 +3,170 @@ const User = require("../models/User");
 const Comment = require("../models/Comment");
 
 const PostController = {
-  async create(req, res, next) {
-    try {
-      const post = await Post.create({
-        ...req.body,
-        userId: req.user._id,
-        image: req.file.filename,
-      });
-      await User.findByIdAndUpdate(
-        req.user._id,
-        { $push: { postIds: post._id } },
-        { new: true }
-      );
+	async create(req, res, next) {
+		try {
+			const post = await Post.create({
+				...req.body,
+				userId: req.user._id,
+				images: req.file.filename,
+			});
+			await User.findByIdAndUpdate(
+				req.user._id,
+				{
+					$push: {
+						postIds: post._id,
+					},
+				},
+				{ new: true }
+			);
 
-      res.status(201).send(post);
-    } catch (error) {
-      console.error(error);
-      next(error);
-    }
-  },
+			res.status(201).send(post);
+		} catch (error) {
+			console.error(error);
+			next(error);
+		}
+	},
 
-  async update(req, res) {
-    try {
-      const post = await Post.findByIdAndUpdate(req.params._id, req.body, {
-        new: true,
-      });
-      res.send({ message: "Post updated successfully!", post });
-    } catch (error) {
-      console.error(error);
-    }
-  },
+	async update(req, res) {
+		try {
+			const post = await Post.findByIdAndUpdate(req.params._id, req.body, {
+				new: true,
+			});
+			res.send({ message: "Post updated successfully!", post });
+		} catch (error) {
+			console.error(error);
+		}
+	},
 
-  async delete(req, res) {
-    try {
-      await Post.findByIdAndDelete(req.params._id);
-      await Comment.deleteMany({ postId: req.params._id });
-      res.send({ message: "Post deleted succesfully" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: "Error trying to remove the post" });
-    }
-  },
+	async delete(req, res) {
+		try {
+			await Post.findByIdAndDelete(req.params._id);
+			await Comment.deleteMany({ postId: req.params._id });
+			res.send({ message: "Post deleted successfully" });
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ message: "Error trying to remove the post" });
+		}
+	},
 
-  async getAll(req, res, next) {
-    try {
-      const { page = 1, limit = 10 } = req.query;
-      const posts = await Post.find({})
-        .populate({
-          path: "userId",
-          select: "username",
-        })
-        .populate({
-          path: "commentIds",
-          populate: {
-            path: "userId",
-            select: "username",
-          },
-        })
-        .limit(limit)
-        .skip((page - 1) * limit);
-      res.status(200).send(posts);
-    } catch (error) {
-      console.error(error);
-      next(error);
-    }
-  },
+	async getAll(req, res, next) {
+		try {
+			const { page = 1, limit = 10 } = req.query;
+			const posts = await Post.find({})
+				.populate({ path: "userId", select: "username" })
+				.populate({
+					path: "commentIds",
+					populate: {
+						path: "userId",
+						select: "username",
+					},
+				})
+				.limit(limit)
+				.skip((page - 1) * limit);
+			res.status(200).send(posts);
+		} catch (error) {
+			console.error(error);
+			next(error);
+		}
+	},
 
-  async getById(req, res) {
-    try {
-      const post = await Post.findById(req.params._id);
-      res.send(post);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: "There was a problem getting the post" });
-    }
-  },
+	async getById(req, res) {
+		try {
+			const post = await Post.findById(req.params._id);
+			res.send(post);
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ message: "There was a problem getting the post" });
+		}
+	},
 
-  async getByName(req, res) {
-    try {
-      if (req.params.name.length > 20) {
-        return res.status(400).send("Search too long");
-      }
-      const title = new RegExp(req.params.name, "i");
-      const posts = await Post.find({ title });
-      res.send(posts);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: "There was a problem getting the post" });
-    }
-  },
+	async getByTitle(req, res) {
+		try {
+			if (req.params.title.length > 20) {
+				return res.status(400).send("Search too long");
+			}
+			const title = new RegExp(req.params.title, "i");
+			const posts = await Post.find({ title });
+			res.send(posts);
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ message: "There was a problem getting the post" });
+		}
+	},
 
-  async like(req, res) {
-    try {
-      const loggedUser = await User.findById(req.user._id);
-      let postToLike = await Post.findById(req.params._id);
+	async like(req, res) {
+		try {
+			const loggedUser = await User.findById(req.user._id);
+			let postToLike = await Post.findById(req.params._id);
 
-      if (!postToLike)
-        return res.status(400).send({ message: "Post not found" });
+			if (!postToLike)
+				return res.status(400).send({ message: "Post not found" });
 
-      if (postToLike.likes.includes(req.user._id)) {
-        return res.status(400).send({
-          message: `You already liked ${postToLike.userId}, post ${loggedUser.username}`,
-        });
-      } else {
-        postToLike = await Post.findByIdAndUpdate(
-          req.params._id,
-          { $push: { likes: req.user._id } },
-          { new: true }
-        );
-        await User.findByIdAndUpdate(
-          req.user._id,
-          { $push: { likesList: req.params._id } },
-          { new: true }
-        );
-        res.send(postToLike);
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: "There was a problem liking the post" });
-    }
-  },
+			if (postToLike.likes.includes(req.user._id)) {
+				return res
+					.status(400)
+					.send({
+						message: `You already liked ${postToLike.userId}, post ${loggedUser.username}`,
+					});
+			} else {
+				postToLike = await Post.findByIdAndUpdate(
+					req.params._id,
+					{
+						$push: {
+							likes: req.user._id,
+						},
+					},
+					{ new: true }
+				);
+				await User.findByIdAndUpdate(
+					req.user._id,
+					{
+						$push: {
+							likesList: req.params._id,
+						},
+					},
+					{ new: true }
+				);
+				res.send(postToLike);
+			}
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ message: "There was a problem liking the post" });
+		}
+	},
 
-  async unlike(req, res) {
-    try {
-      let postToUnlike = await Post.findById(req.params._id);
-      if (!postToUnlike)
-        return res.status(400).send({ message: "Post not found" });
+	async unlike(req, res) {
+		try {
+			let postToUnlike = await Post.findById(req.params._id);
+			if (!postToUnlike)
+				return res.status(400).send({ message: "Post not found" });
 
-      postToUnlike = await Post.findByIdAndUpdate(
-        req.params._id,
-        { $pull: { likes: req.user._id } },
-        { new: true }
-      );
-      await User.findByIdAndUpdate(
-        req.user._id,
-        { $pull: { likesList: req.params._id } },
-        { new: true }
-      );
-      res.send(postToUnlike);
-    } catch (error) {
-      console.error(error);
-      res
-        .status(500)
-        .send({ message: "There was a problem unliking the post" });
-    }
-  },
+			postToUnlike = await Post.findByIdAndUpdate(
+				req.params._id,
+				{
+					$pull: {
+						likes: req.user._id,
+					},
+				},
+				{ new: true }
+			);
+			await User.findByIdAndUpdate(
+				req.user._id,
+				{
+					$pull: {
+						likesList: req.params._id,
+					},
+				},
+				{ new: true }
+			);
+			res.send(postToUnlike);
+		} catch (error) {
+			console.error(error);
+			res
+				.status(500)
+				.send({ message: "There was a problem unliking the post" });
+		}
+	},
 };
 
 module.exports = PostController;
--- routes/posts.js
+++ routes/posts.js
@@ -3,19 +3,24 @@ const router = express.Router();
 const PostController = require("../controllers/PostController");
 
 const {
-  authentication,
-  isAdmin,
-  isSuperAdmin,
-  isPostAuthor,
+	authentication,
+	isAdmin,
+	isSuperAdmin,
+	isPostAuthor,
 } = require("../middleware/authentication");
 const upload = require("../middleware/upload");
 
-router.post("/", authentication, upload.single("image"), PostController.create);
+router.post(
+	"/",
+	authentication,
+	upload.array("images", 10),
+	PostController.create
+);
 router.put("/:_id", authentication, isPostAuthor, PostController.update);
 router.delete("/:_id", authentication, isPostAuthor, PostController.delete);
 router.get("/", PostController.getAll);
 router.get("/:_id", PostController.getById);
-router.get("/name/:name", PostController.getByName);
+router.get("/title/:title", PostController.getByTitle);
 router.put("/like/:_id", authentication, PostController.like);
 router.put("/unlike/:_id", authentication, PostController.unlike);
 
